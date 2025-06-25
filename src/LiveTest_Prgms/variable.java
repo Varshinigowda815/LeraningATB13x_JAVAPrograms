0d1880c44ae094e779a629cabf2a3dc18d9d3898
Varshinigowda815 <varshinigowda815@gmail.com>
2025-06-25 21:11:36 +0530

    Add

---
+++ src/LiveTest_Prgms/variable.java
@@ -0,0 +1,22 @@
+package LiveTest_Prgms;
+
+public class variable {
+    public static void main(String[] args) {
+
+
+        int age = 25;
+
+        double salary = 50250.75;
+
+        boolean isEmployeePermanent = true;
+
+
+        char grade = 'A';
+
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: ₹" + salary);
+        System.out.println("Is Employee Permanent: " + isEmployeePermanent);
+        System.out.println("Grade: " + grade);
+    }
+}
